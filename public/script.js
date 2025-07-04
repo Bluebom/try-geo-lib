131383a58ae1decfd9e50b059e6dcfd2396885ab
franklin <franklinhpf@hotmail.com>
2025-07-04 18:02:15 -0300

    Update map view and marker positions; add missing icon assets

--- public/script.js
+++ public/script.js
@@ -1,13 +1,13 @@
 (function () {
     const L = LeafletGeo;
 
-    const map = L.map('map').setView([-7.1195, -34.8451], 13);
+    const map = L.map('map').setView([-14.235, -51.925], 5);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    L.marker([-7.1195, -34.8451]).addTo(map)
+    L.marker([-7.1167, -34.8667]).addTo(map)
         .bindPopup('<b>Hello from João Pessoa!</b><br>My custom library works!')
         .openPopup();
 })();
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,10 @@
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
+
+L.Icon.Default.mergeOptions({
+  iconUrl: '/img/marker-icon.png',
+  iconRetinaUrl:  '/img/marker-icon-2x.png',
+  shadowUrl:  '/img/marker-shadow.png',
+});
+
 export default L;
